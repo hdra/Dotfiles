553b8ba9fa059fe19a414b14f9ca6f7cefe1b363
Hendra <hendra2392@gmail.com>
2013-06-04 14:03:59 +0800

    reset

--- i3/lcd-on-left.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xrandr --output CRT1 --auto --left-of LVDS
-xrandr --output CRT1 --primary
-~/.i3/config/set-wall.sh
--- i3/set-wall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-feh --bg-fill /media/ent/Wallpapers/Walls4/Dx5P0.jpg
