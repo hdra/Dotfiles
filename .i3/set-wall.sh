fd2496ef34b155b5c1c655547798d07126c43965
Hendra <hendra2392@gmail.com>
2013-06-06 15:14:22 +0800

    fixed i3 shell script path

--- .i3/set-wall.sh
+++ .i3/set-wall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-feh --bg-fill /media/ent/Wallpapers/Walls4/Dx5P0.jpg
+feh --bg-fill /media/ent/Wallpapers/Walls4/swimming___hd_by_ether-d3b69h7.jpg
+#feh --bg-fill /media/ent/Wallpapers/Walls4/Dx5P0.jpg
