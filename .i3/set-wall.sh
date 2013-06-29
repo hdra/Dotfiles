b9f693a8204dc3db5f842f9fca737baa3f67dd10
Hendra <hendra2392@gmail.com>
2013-06-29 14:59:53 +0800

    update

--- .i3/set-wall.sh
+++ .i3/set-wall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-feh --bg-fill /media/ent/Wallpapers/Walls4/swimming___hd_by_ether-d3b69h7.jpg
+feh --bg-fill /media/ent/Wallpapers/Walls3/batman.jpg
 #feh --bg-fill /media/ent/Wallpapers/Walls4/Dx5P0.jpg
