402857c4010c9f90e30222a6b2336452529a7ba8
Hendra <hendra2392@gmail.com>
2013-07-02 16:09:32 +0800

    add linking script

---
+++ linkem.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+shopt -s dotglob extglob globstar nullglob
+
+#git submodule update --init
+
+if [[ $0 == /* ]]; then
+    location=${0%/*}
+else
+    location=$PWD/${0#./}
+    location=${location%/*}
+fi
+
+excludes='+(README.md|boostrap.sh|linkem.sh|.git|.git/*|*/.git|.gitignore|.gitmodules)'
+
+echo "Linking.."
+
+for file in "$location"/**/*; do
+    bare="${file#$location/}"
+
+    case "$bare" in
+        $excludes) continue ;;
+    esac
+
+    if [[ -d "$file" ]]; then
+        mkdir -p ~/"$bare"
+    elif [[ -f "$file" ]]; then
+        ln -sfn "$file" ~/"$bare"
+    fi
+done
+
+echo "Done!"
